2f090c0421f628d1ba3b28e93cb12c21b8c20f21
Jonathan Kuhl <jonathankuhl@Jonathans-MBP-2.maine.rr.com>
2018-12-21 13:18:12 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Web App Generator
+
+This is a simple web app generator to construct a file directory skeleton for a web app.  It will set up directories for your html, css and javascript files, initialize a git repository (if you decide to) and install any dependencies you desire.
+
+This is designed for basic web pages.  If you're creating something in Vue or React or using the Express generator, you'll be better off using their CLI tools.
+
+If you have VSCode installed, it will, if prompted, open the files in VSCode so you can get started.  If you have live-server installed, it will, if prompted, run the live-server dev server.
+
+## Basic usage
+
+Minimal use:
+
+    webapp myapp
+
+This will create an application with the following structure:
+
+    /myapp 
+        /html
+        /scripts
+            app.js
+        /style
+            style.css
+        index.html
+
+`index.html` is prepopulated with basic html 5 boiler plate and already has `<link>` tags and `<script>` tags for your CSS and JavaScript
+
+Any arguments after the name of the application will be NPM packages.  The generator will create a package.json file and then install all the NPM packages.
+
+    webapp myapp jquery lodash date-fns
+
+This will create an application called `myapp` and install jquery, lodash and date-fns.  If there is a git repo, this will also generate a `.gitignore` with `/node_modules` ignored.
+
+## Installation
+
+Download the .sh file, put in bin directory and export.
---
+++ webapp.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+if [ -e $1 ]; then
+    echo $1 'already exists!'
+    exit
+else
+    echo 'Generating' $1 '. . .' 
+    mkdir $1
+    cd $1
+    mkdir 'html'
+    mkdir 'scripts'
+    touch 'scripts/app.js'
+    mkdir 'style'
+    touch 'style/style.css'
+    touch 'index.html'
+    echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>' > 'index.html'
+    read -p 'Do you want a git repo? [Y/N]' response
+    if [ $response = 'Y' ] || [ $response = 'y' ]; then
+        echo 'Generating git repo . . .'
+        git init
+        git add .
+        git commit -m 'initial commit'
+        read -p 'Please enter the url to your git (.git):' url
+        git remote add origin $url
+        git push -u origin master
+    fi
+fi
+
+if [ $# -eq 1 ]; then
+    read -p 'Do you want a package.json? [Y/N] ' response
+    if [ $response = 'Y' ] || [ $response = 'y' ]; then
+        npm init
+    fi
+else 
+    npm init
+    shift
+    echo 'installing' $*
+    npm install $*
+    touch '.gitignore'
+    echo '/node_modules' > '.gitignore'
+fi
+
+read -p 'Would you like to open VSCode? [Y/N] ' response
+if [ $response = 'Y' ] || [ $response = 'y' ]; then
+    code .
+fi
+
+echo 'Your web app has been generated!'
+echo 'You can find it at'
+pwd
+
+read -p 'Would you like to start live-server? [Y/N] ' response
+if [ $response = 'Y' ] || [ $response = 'y' ]; then
+    live-server
+fi
+
+exit
